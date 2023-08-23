3962db308813ad045edc8c9f76fdb8efe5f37b2e
dereck lhw <dereck.lamhonwah@gmail.com>
2023-08-23 22:14:14 +0400

    feat: all message contatenate into one

--- notifications/whatsapp.py
+++ notifications/whatsapp.py
@@ -2,19 +2,20 @@ import pywhatkit as kit
   
 def send_message(msg_list, phone_number):
     if msg_list:
+        concantenate_msg = ""
         for msg in msg_list:
-            msg = f"Date: {msg['date']} | Bank: {msg['url']} | Country: {msg['country']} | Value: {msg['value']}"
-            try:
-                kit.sendwhatmsg_instantly(
-                    phone_no=phone_number,
-                    message=msg,
-                    wait_time=10,
-                    tab_close=True,
-                    close_time=1
-                )
-            except Exception as e:
-                print(e)
-                return
-        print("All messages sent successfully!")
+            concantenate_msg += f"Date: {msg['date']} | Bank: {msg['url']} | Country: {msg['country']} | Value: {msg['value']}\n\n"
+        try:
+            kit.sendwhatmsg_instantly(
+                phone_no=phone_number,
+                message=concantenate_msg,
+                wait_time=15,
+                tab_close=True,
+                close_time=1
+            )
+            print("All messages sent successfully!")
+        except Exception as e:
+            print(e)
+            return
     else:
         print("No messages to send.")
